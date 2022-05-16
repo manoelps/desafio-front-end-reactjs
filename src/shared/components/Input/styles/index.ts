e258d1a0fba95b5506a92aa1a3bb2da897600b2e
Manoel Pereira dos Santos <manoelps@live.com>
2022-05-15 22:08:43 -0300

    Feito ajustes no placeholder

--- src/shared/components/Input/styles/index.ts
+++ src/shared/components/Input/styles/index.ts
@@ -1,52 +1,6 @@
 import styled from "styled-components";
 import globalConfig from "../../../../utils";
 
-// interface IInputText {
-//     error?: boolean;
-// }
-
-// export const Container = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     margin: 10px 0 10px 0;
-
-//     align-items: center;
-// `;
-
-// export const InputContainer = styled.div``;
-
-// export const Title = styled.div`
-//     font-size: 18px;
-//     color: ${({ theme }) => theme.gray};
-// `;
-
-// export const InputText = styled.input<IInputText>`
-//     width: 300px;
-//     padding: 5px 0px 2px 5px;
-//     border: none;
-//     //border-bottom: ${({ error, theme }) => (error ? `2px solid ${theme.red}` : `2px solid ${theme.gray2}`)};
-//     border-bottom: ${({ error, theme }) => (error ? `1px solid ${theme.red}` : `1px solid ${theme.gray1}`)};
-//     box-sizing: border-box;
-//     resize: vertical;
-//     font-size: 20px;
-//     font-weight: bold;
-//     //color: ${({ theme }) => theme.gray2};
-//     color: ${({ theme }) => theme.black};
-//     :focus {
-//         outline: none;
-//         color: ${({ theme }) => theme.gray1};
-//     }
-
-//     border: 1px solid #000;
-// `;
-
-export const Message = styled.div`
-    margin-top: 5px;
-    color: ${({ theme }) => theme.red};
-    text-align: center;
-`;
-//////////////////////
-
 interface IInputText {
     error?: boolean;
     readonly?: boolean;
@@ -123,4 +77,10 @@ export const ContainerButton = styled.div`
         flex-direction: row;
         margin: 30px 0;
     }
+`;
+
+export const Message = styled.div`
+    margin-top: 5px;
+    color: ${({ theme }) => theme.red};
+    text-align: center;
 `;
